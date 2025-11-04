f3d208b09032bc5988bc5b4e7f98875aecde6b37
Ficarra-Gabriele <ficarra.gabriele@franchettisalviani.net>
2025-11-04 10:54:52 +0100

    finale

--- MainAggregazione/src/mainaggregazione/Brano.java
+++ MainAggregazione/src/mainaggregazione/Brano.java
@@ -10,5 +10,20 @@ package mainaggregazione;
  * @author ficarra.gabriele
  */
 class Brano {
+    private String titolo;
+    private String artista;
+    private int durataSecondi;
     
+    public Brano(String titolo, String artista,int durataSecondi){
+        this.artista = artista;
+        this.durataSecondi = durataSecondi;
+        this.titolo = titolo;
+    }
+    
+    
+    
+    @Override
+    public String toString() {
+    return titolo + artista + durataSecondi;
+    }
 }
--- MainAggregazione/src/mainaggregazione/Playlist.java
+++ MainAggregazione/src/mainaggregazione/Playlist.java
@@ -5,10 +5,34 @@
  */
 package mainaggregazione;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author ficarra.gabriele
  */
 class Playlist {
+    private String nome;
+    private String proprietario;
+    ArrayList brani = new ArrayList<Brano>();
+    private Brano brano;
     
+    public Playlist(String nome, String proprietario, String titolo, String artista, int durataSecondi ){
+        this.nome = nome;
+        this.proprietario = proprietario;
+        this.brano = new Brano(titolo, artista, durataSecondi);
+    }
+    
+    void aggiungi(Brano brano){
+        brani.add(brano);
+    }
+    
+    public Brano getBrani(){
+        return brano;
+    }
+    
+    @Override
+    public String toString() {
+    return nome + proprietario + brano ;
+    }
 }
